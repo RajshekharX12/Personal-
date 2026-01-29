3811759e253f13215e3c69e90054a8d14bba985c
RajshekharX12 <137974797+RajshekharX12@users.noreply.github.com>
2026-01-29 21:34:58 +0530

    Create temp.py

---
+++ hybrid/plugins/temp.py
@@ -0,0 +1,16 @@
+#(©) @Hybrid_Vamp - https://github.com/hybridvamp
+
+class temp(object):
+    BOT_UN = None
+    TEMP_VAR1 = []
+    TEMP_VAR2 = []
+    PAID_LOCK = []
+    INV_DICT = {}
+    PENDING_INV = []
+    NUMBE_RS = []
+    AVAILABLE_NUM = []
+    RENTED_NUMS = []
+    UN_AV_NUMS = []
+    BLOCKED_NUMS = []
+    RESTRICTED_NUMS = []
+    
