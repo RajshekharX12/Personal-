13774a7f6846fabf0a953faa2ce240c3489c9b24
RajshekharX12 <137974797+RajshekharX12@users.noreply.github.com>
2026-02-22 11:58:07 +0530

    Update temp.py

--- hybrid/plugins/temp.py
+++ hybrid/plugins/temp.py
@@ -7,15 +7,15 @@ class temp(object):
     BOT_UN = None
     TEMP_VAR1 = []
     TEMP_VAR2 = []
-    PAID_LOCK = []
+    PAID_LOCK = set()
     INV_DICT = {}
-    PENDING_INV = []
+    PENDING_INV = set()
     NUMBE_RS = []
-    AVAILABLE_NUM = []
-    RENTED_NUMS = []
-    UN_AV_NUMS = []
-    BLOCKED_NUMS = []
-    RESTRICTED_NUMS = []
+    AVAILABLE_NUM = set()
+    RENTED_NUMS = set()
+    UN_AV_NUMS = set()
+    BLOCKED_NUMS = set()
+    RESTRICTED_NUMS = set()
     _lock = asyncio.Lock()
 
     @classmethod
