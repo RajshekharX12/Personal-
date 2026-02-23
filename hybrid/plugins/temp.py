8c0d916da3e0e26ca7adcf7be0e8bf417f44469c
RajshekharX12 <137974797+RajshekharX12@users.noreply.github.com>
2026-02-24 03:04:42 +0530

    Update temp.py

--- hybrid/plugins/temp.py
+++ hybrid/plugins/temp.py
@@ -5,16 +5,14 @@ import asyncio
 
 class temp(object):
     BOT_UN = None
-    TEMP_VAR1 = []
-    TEMP_VAR2 = []
     PAID_LOCK = set()
     INV_DICT = {}
     PENDING_INV = set()
     NUMBE_RS = []
+    NUMBE_RS_SET = set()  # O(1) membership; keep in sync with NUMBE_RS
     AVAILABLE_NUM = set()
     RENTED_NUMS = set()
     UN_AV_NUMS = set()
-    BLOCKED_NUMS = set()
     RESTRICTED_NUMS = set()
     _lock = asyncio.Lock()
 
