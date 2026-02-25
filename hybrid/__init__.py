72d647b3492ee999ebec6bbc5b6496a1f4ec2f02
RajshekharX12 <137974797+RajshekharX12@users.noreply.github.com>
2026-02-26 03:08:54 +0530

    Update __init__.py

--- hybrid/__init__.py
+++ hybrid/__init__.py
@@ -43,8 +43,7 @@ logging.basicConfig(
             LOG_FILE_NAME,
             maxBytes=50000000,
             backupCount=10
-        ),
-        logging.StreamHandler()
+        )
     ]
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
