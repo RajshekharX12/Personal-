9ae7fc006ce20ac3ebe815fa22bcb9f4df5c2cce
RajshekharX12 <137974797+RajshekharX12@users.noreply.github.com>
2026-02-26 03:27:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,6 @@ REDIS_URI = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 DB_NAME = os.environ.get("DATABASE_NAME", "rental")
 CRYPTO_API = os.environ.get("CRYPTO_API", "523718:AAEQO6x6qx2PXerElEVuIvBcuL5rdHgDR4Q")
 TON_WALLET = os.environ.get("TON_WALLET", "UQAYH3MHNSUABi73Z6HwIcuXkmws1tBDDN-lWIPhXZW455bI")  # TON wallet address for Tonkeeper payments
-TON_API_TOKEN = os.environ.get("TON_API_TOKEN", "762cfcfb5dc3b67f31d8eba4fec6377bc98b28f3b8e2a24954d31201e6143911")  # Optional: for payment checker (get from @tonapibot)
 
 # ============== FRAGMENT data =============
 FRAGMENT_API_HASH = os.environ.get("FRAGMENT_API_HASH", "38f80e92d2dbe5065b")
@@ -38,6 +37,7 @@ except ValueError:
     raise Exception("Your Admins list doesn't contain valid integers.")
 
 ADMINS.append(OWNER_ID)
+
 
 
 
